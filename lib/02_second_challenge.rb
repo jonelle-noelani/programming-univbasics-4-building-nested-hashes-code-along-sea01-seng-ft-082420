5cedbb5fe7627fb5b64f85eb1eac6779b9ced6bc
Jonelle Noelani Yacapin <jonelle_noelani@hotmail.com>
2020-08-10 08:07:45 +0000

    Automatically backed up by Learn

--- lib/02_second_challenge.rb
+++ lib/02_second_challenge.rb
@@ -1,13 +1,18 @@
 def second_challenge
   epic_tragedy = {
-    :montague => {:patriarch => { },
+    :montague => {
+:patriarch => { },
 :matriarch => { },
 :hero => { },
 :hero_friends => [ ]
 
-      #your key/value pairs here!
       }, 
     :capulet => {
+:patriarch => { },
+:matriarch => { },
+:heroine => { },
+:heroine_friends => [ ]
+
       #your key/value pairs here!
       }}
 
